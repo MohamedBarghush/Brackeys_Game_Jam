cc3d78f2019455f5b97c6c63cb59da781966d3c8
Mohamed ElShab <cinosmoh@gmail.com>
2025-02-18 17:43:37 +0200

    Basic Controller

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using StarterAssets;
+
+interface IInteractable {
+    public void Interact();
+}
+
+public class Interactor : MonoBehaviour
+{
+    [SerializeField] private LayerMask interactionLayer;
+    [SerializeField] private float rayDistance = 10f;
+
+    private StarterAssetsInputs _inputs;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _inputs = GetComponent<StarterAssetsInputs>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (_inputs.interact == true) {
+            if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, rayDistance, interactionLayer)) {
+                if (hit.collider.TryGetComponent<IInteractable>(out var interactable)) {
+                    interactable.Interact();
+                }
+            }
+            _inputs.interact = false;
+        }
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * rayDistance);
+    }
+}
